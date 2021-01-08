35fcb67b753e6297b523b6b8c975073dd025c37f
Anjolaoluwa Ogunmefun <ogunmefunanjola@gmail.com>
2021-01-08 13:59:10 +0100

    few changes

--- server.js
+++ server.js
@@ -1,32 +1,52 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const download = require('download')
+const FileType = require('file-type');
+const path = require('path')
+const mime = require('mime-types')
+
 const app = express();
 
 const port = 6000;
-const csv = require('csv-parser')
+const csv = require('csvtojson')
 const fs = require('fs-extra')
 const results = [];
 
+app.use(bodyParser.json());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    console.log('it works')
-    res.send("hey")
+// app.get('/', (req, res) => {
+//     console.log('it works')
+//     res.send("hey")
+// })
+
+app.post('/', async (req, res) => {
+    const { select_field, url }= req.body.csv
+    if(!url){
+        return res.send({
+            error: true,
+            message:"url cannot be empty!"
+        })
+    }
+    await download(url, "downloads" , {
+        filename:"data.csv"
+    });
+
+  const mimeType = mime.contentType('downloads/data.csv') 
+  console.log('feed back', mimeType);
+    if(mimeType !== 'text/csv'){
+        return res.send({
+            error: true,
+            message: "Url does not contain a valid csv file"
+        })
+    }
+
+    const jsonArray=await csv().fromFile('downloads/data.csv');
+    console.log({jsonArray})
+
+
+    console.log('result', req.body.csv)
 })
-
-app.post('/')
-
-
-
-// fs.createReadStream('data.csv')
-//   .pipe(csv())
-//   .on('data', (data) => results.push(data))
-//   .on('end', () => {
-//     console.log(results);
-//     // [
-//     //   { NAME: 'Daffy Duck', AGE: '24' },
-//     //   { NAME: 'Bugs Bunny', AGE: '22' }
-//     // ]
-//   });
 
 app.listen(port, () => {
     console.log(`App is listening now on port ${port}`)
